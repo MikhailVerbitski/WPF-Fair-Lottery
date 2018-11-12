1c9d23484336a2721fc65ad05cc28469bb4f28dd
Mikhail <misha.verbi@gmail.com>
2018-11-12 18:47:20 +0300

    version 2

---
+++ Fair Lottery (Version 2.0)/Games.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fair_Lottery__Version_2._0_
+{
+    interface Game
+    {
+        void Run(Persone persona);
+        int Raffle(Persone persona);
+        int MakeBet(Persone persona);
+    }
+    class Dice : Game
+    {
+        public Dice()
+        {
+
+        }
+        public void Run(Persone persona)
+        {
+            int ID_Raffle = Raffle(persona,1);
+            int ID_Bet = MakeBet(persona, ID_Raffle, 1);
+            Result(persona, ID_Raffle, ID_Bet, 2);
+            Visual.ShowResult(ID_Bet, "Dice"); 
+        }
+        private int Raffle(Persone persona, int RoomThrow)
+        {
+            return Table.Raffle.CreateRaffle(persona.sID_Persone, new Random().Next(1, Table.Items.GetCountInGame("Dice") * RoomThrow ));
+        }
+        private int MakeBet(Persone persona, int ID_of_Raffle, int RoomThrow)
+        {
+            Pair<int,int> IdAndNum = Visual.ShowAndSelectItemDice();
+            return Table.Bet.CreateBet(IdAndNum.First, ID_of_Raffle, Visual.MakeBet(), IdAndNum.Second);
+        }
+        private void Result(Persone persona, int ID_Raffle, int ID_Bet, decimal ratio)
+        {
+            decimal size = Table.Bet.GetSize(ID_Bet);
+            decimal result = (Table.Raffle.GetWinNumber(ID_Raffle) == Table.Bet.GetNumber(ID_Bet)) ? (size * ratio) : -size;
+            Table.Persone.UpdateMoney(persona.sID_Persone, result);
+            Table.Raffle.SetResult(ID_Raffle, result);
+        }
+        private void Throw()
+        {
+
+        }
+
+        public int Raffle(Persone persona)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int MakeBet(Persone persona)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Lottery : Game
+    {
+        public Lottery()
+        {
+
+        }
+        public void Run(Persone persone)
+        {
+            int ID_Raffle = Raffle(persone);     //выбор выйгрышного
+            //
+                //покупка билетов
+            //еще?
+            //рузультат
+            //отображение
+        }
+        private int Raffle(Persone persone)
+        {
+            return Table.Raffle.CreateRaffle(persone.sID_Persone, new Random().Next(1, 1000000));
+        }
+        private int MakeBet(Persone persone, int ID_of_Raffle, int RoomThrow)
+        {
+            Pair<int, int> IdAndNum = Visual.ShowAndSelectItemLottery();
+            //return Table.Bet.CreateBet(IdAndNum.First, ID_of_Raffle, Visual.MakeBet(), IdAndNum.Second);
+        }
+        private int Result(Persone persone)
+        {
+
+        }
+
+        int Game.Raffle(Persone persona)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int MakeBet(Persone persona)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    //class Cups : Game
+    //{
+    //    public Cups()
+    //    {
+
+    //    }
+    //    public void Run(Persone persona)
+    //    {
+                //выбор выйгрышного
+                //покупка билетов
+                //рузультат
+                //отображение
+    //    }
+    //}
+}
---
+++ Fair Lottery (Version 2.0)/TableObject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fair_Lottery__Version_2._0_
+{
+    interface TableObject
+    {
+        void Refresh();
+    }
+    class Persone : TableObject
+    {
+        int ID_Persone;
+        public int sID_Persone { get { return ID_Persone; } }
+        string Name;
+        string Pass;
+        decimal Money;
+        private Persone(int ID_Persone, string Name, string Pass, decimal Money)
+        {
+            this.ID_Persone = ID_Persone;
+            this.Name = Name;
+            this.Pass = Pass;
+            this.Money = Money;
+        }
+        public void Refresh()
+        {
+            Table.Persone.FindPersone(ID_Persone, out Name, out Pass, out Money);
+        }
+        public static Persone GetPersone(int ID_Persone)
+        {
+            string name;
+            string pass;
+            decimal money;
+            Table.Persone.FindPersone(ID_Persone, out name, out pass, out money);
+            return new Persone(ID_Persone, name, pass, money);
+        }
+        public static Persone CreatePersone(string Name, string Pass, decimal Money)
+        {
+            int id = Table.Persone.CreatePersone(Name, Pass, Money);
+            return GetPersone(id);
+        }
+    }
+}
